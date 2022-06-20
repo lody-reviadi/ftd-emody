87cb476e3e2c0b1871b8b3eb3f4c395b84833493
devon-sulistio <devon-sulistio@kayac.com>
2022-06-20 10:13:40 +0900

    (difficulty) 初コミット

---
+++ Assets/Scripts/DifficultySelectScene/DifficultySelectPresenter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelectPresenter : MonoBehaviour
+{
+    private DifficultySelectView _view;
+    public DifficultySelectModel model;
+}
