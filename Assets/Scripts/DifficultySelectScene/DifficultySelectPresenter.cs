cce9d388d1fbe1ce19b8a6b7ef91e5857a198e39
devon-sulistio <devon-sulistio@kayac.com>
2022-06-27 12:51:09 +0900

    (title-screen) DifficultySelectScene削除

--- Assets/Scripts/DifficultySelectScene/DifficultySelectPresenter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DifficultySelectPresenter : MonoBehaviour
-{
-    private DifficultySelectView _view;
-    public DifficultySelectModel model;
-}
